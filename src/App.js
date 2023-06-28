b7855f3cf4d9f5e463ae6f1924729b40a96dc775
Clayton Vass <clayvass@gmail.com>
2023-06-28 10:04:21 -0500

    11

--- src/App.js
+++ src/App.js
@@ -1,23 +1,14 @@
 import React, { Component } from "react"
 import "./App.css"
-import Title from './Title'
-import Home from './pages/home';
-import About from './pages/about';
-import Events from './pages/events';
+import Navbar from './components/Navbar'
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
 class App extends Component {
   render() {
     return (
-
-      <Router>
-        <Title />
-        <Routes>
-          <Route path ='/' component={Home} />
-          <Route path ='/about' component={About} />
-          <Route path ='/events' component={Events} />
-        </Routes>
-      </Router>
+      <div className="App">
+        <Navbar/>
+      </div>
     )
   }
 }
