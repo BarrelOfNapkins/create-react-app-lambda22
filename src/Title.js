aeb1a984c77594440fded2abf8ea549b96991b8a
Clayton Vass <clayvass@gmail.com>
2023-06-28 08:49:21 -0500

    4

--- src/Title.js
+++ src/Title.js
@@ -2,10 +2,22 @@
 import './Title.css';
 import React from 'react'
 
+const handleItemClick = (item) => {
+  // Handle click logic here
+  console.log(`Clicked ${item}`);
+};
+
 function Title() {
   return (
     <div>
-      <h1>SFPOPOS</h1>
+      <h1>my website</h1>
+      <nav>
+        <ul>
+          <li>
+          <button onClick={() => handleItemClick('Home')}>Home</button>
+          </li>
+        </ul>
+      </nav>
     </div>
   )
 }
