deb9e999f9686790e7589070be5224f2de775c1a
Clayton Vass <clayvass@gmail.com>
2023-06-28 09:40:42 -0500

    11

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import "./App.css"
 import Title from './Title'
 import Home from './pages/home';
 import About from './pages/about';
+import Events from './pages/events';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 /*
 class LambdaDemo extends Component {
@@ -54,6 +55,7 @@ class App extends Component {
         <Routes>
           <Route path ='/' component={Home} />
           <Route path ='/about' component={About} />
+          <Route path ='/events' component={Events} />
         </Routes>
       </Router>
     )
