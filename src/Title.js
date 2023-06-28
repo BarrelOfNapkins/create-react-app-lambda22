f95fd3c8c972a922fd81b8e75723d182eaa48a00
Clayton Vass <clayvass@gmail.com>
2023-06-28 09:19:53 -0500

    9

--- src/Title.js
+++ src/Title.js
@@ -2,20 +2,25 @@
 import './Title.css';
 import React from 'react'
 import {
+  Nav,
   NavLink,
   NavMenu,
 } from './NavbarElements';
 
 function Title() {
   return (  
-    <NavMenu>
-      <NavLink to='/test1' activeStyle>
-        test1
-      </NavLink>
-      <NavLink to='/test2' activeStyle>
-        test2
-      </NavLink>
-    </NavMenu>
+    <>
+      <Nav>
+        <NavMenu>
+          <NavLink to='/test1' activeStyle>
+            test1
+          </NavLink>
+          <NavLink to='/test2' activeStyle>
+            test2
+          </NavLink>
+        </NavMenu>
+      </Nav>
+    </>
   )
 }
 
